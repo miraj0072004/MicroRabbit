dee39690ae97a3fa6e4ca802fb5f55fb2a40b5dc
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-09-01 23:20:35 +1000

    Banking ApplicationLayer,IOC Container and AppSettings updates

---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository: IAccountRepository
+    {
+        private readonly BankingDbContext _bankingDbContext;
+
+        public AccountRepository(BankingDbContext bankingDbContext)
+        {
+            _bankingDbContext = bankingDbContext;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _bankingDbContext.Accounts;
+        }
+    }
+}
